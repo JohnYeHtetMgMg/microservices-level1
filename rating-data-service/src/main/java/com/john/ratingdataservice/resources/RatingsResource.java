ee049b1fe8a93880d6876961e2be084997677589
John <johnyehtetmgmg@gmail.com>
2023-02-12 15:09:23 +0630

    rating-data-service

---
+++ rating-data-service/src/main/java/com/john/ratingdataservice/resources/RatingsResource.java
@@ -0,0 +1,15 @@
+package com.john.ratingdataservice.resources;
+
+import com.john.ratingdataservice.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+}
